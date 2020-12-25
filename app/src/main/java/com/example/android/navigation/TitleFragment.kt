86b612fb65944fc6becfcb9920745477f02360c7
googlicus <googlicus@gmail.com>
2020-12-25 17:55:27 +0500

    lifecycle logging

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,6 +1,8 @@
 package com.example.android.navigation
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -22,9 +24,9 @@ class TitleFragment : Fragment() {
             view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
         setHasOptionsMenu(true)
+        Log.i("TitleFragment","onCreateView()")
         return binding.root
     }
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.options_menu, menu)
@@ -32,5 +34,44 @@ class TitleFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("TitleFragment", "onAttach called")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("TitleFragment", "onCreate called")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("TitleFragment", "onViewCreated called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("TitleFragment", "onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("TitleFragment", "onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("TitleFragment", "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("TitleFragment", "onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("TitleFragment", "onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("TitleFragment", "onDetach called")
     }
 }
